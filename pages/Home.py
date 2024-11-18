6d7d9ebe37a2240ed37aa0b673a5a28a5d20acb5
Muhammad Airell Rahman Pramono <rahmanairell@gmail.com>
2024-11-19 00:50:43 +0800

    V3

--- pages/Home.py
+++ pages/Home.py
@@ -112,7 +112,7 @@ with st.container():
                 file_path = os.path.join("wardrobe_img", filename)
                 
                 # Save the image to the specified folder
-                img.save(file_path)
+                img.save("wardrobe_img")
                 m.store_fit(current_fit)
                 st.write("Added successfully!")
 
